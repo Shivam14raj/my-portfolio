1de42d817d5ce8b15a838b8989da81657b742b23
Shivam14raj <shivamrraj2005@gmail.com>
2026-02-16 00:04:15 +0530

    enhanced landing page

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import photo from "../assets/photo.png";
 import "./Home.css";
 import globe from "../assets/globeimg.png"
+import express from "../assets/express-logo.png"
+import reactimg from "../assets/react-logo.png"
+import git from "../assets/git-logo.png"
+import nextjs from "../assets/nextjs-logo.png"
+
 function Home() {
   return (
     <div>
@@ -21,6 +26,13 @@ function Home() {
          </div>
 
       </div> 
+
+      <div className="logo-container">
+            <img src= {express} alt="" className="express" />
+            <img src= {reactimg} alt=""  className="reactimg"/>
+            <img src= {git} alt=""  className="git"/>
+            <img src= {nextjs} alt=""  className="nextjs"/>
+      </div>
 
       
       
