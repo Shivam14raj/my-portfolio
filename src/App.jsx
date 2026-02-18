b67796ebe75b1b1ba26d7d3416bd1b59dfd7cdf2
Shivam14raj <shivamrraj2005@gmail.com>
2026-02-18 20:31:33 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 import NavBar from './components/NavBar';
 import About from './components/About';
-import Projects from './components/projects';
+import Projects from './components/Projects';
 import Contact from './components/contact';
 function App() {
   return (
